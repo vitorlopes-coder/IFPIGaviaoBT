25789021b124f7fead301414275b9bbbc5804d3c
Vitor Lopes <vcoder00@gmail.com>
2026-09-24 12:18:52 -0300

    refactor(app): conecta rotas do Expo Router exclusivamente as Views

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,7 +4,7 @@ import { StatusBar } from "expo-status-bar";
 export default function RootLayout() {
   return (
     <>
-      <StatusBar style="light" backgroundColor="#501673" />
+      <StatusBar style="light" />
       <Stack
         screenOptions={{
           headerShown: false,
